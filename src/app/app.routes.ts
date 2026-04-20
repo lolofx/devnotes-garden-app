fd6ecea9fcc0114170cd5735b4063abe509d5945
Loïc Leplomb <lolofx@msn.com>
2026-04-20 18:53:09 +0200

    feat(routing): ajouter routes /tags et /tags/:tagName

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,15 @@ export const routes: Routes = [
     loadComponent: () => import('./presentation/pages/note/note.page').then((m) => m.NotePage),
   },
   {
+    path: 'tags',
+    loadComponent: () =>
+      import('./presentation/pages/tags/tags-list.page').then((m) => m.TagsListPage),
+  },
+  {
+    path: 'tags/:tagName',
+    loadComponent: () => import('./presentation/pages/tags/tag.page').then((m) => m.TagPage),
+  },
+  {
     path: '**',
     loadComponent: () =>
       import('./presentation/pages/not-found/not-found.page').then((m) => m.NotFoundPage),
